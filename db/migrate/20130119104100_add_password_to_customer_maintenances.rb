1106a5c7872c8af7fbc00630ddeff5aad390e0c3
John Maenard V. Elauria <johnelauria@hotmail.com>
2013-01-21 13:03:32 +0800

    corrected password strings for postgre migrations

--- db/migrate/20130119104100_add_password_to_customer_maintenances.rb
+++ db/migrate/20130119104100_add_password_to_customer_maintenances.rb
@@ -1,6 +1,6 @@
 class AddPasswordToCustomerMaintenances < ActiveRecord::Migration
   def change
-    add_column :customer_maintenances, :password_digest, :String
-    add_column :customer_maintenances, :remember_token, :String
+    add_column :customer_maintenances, :password_digest, :string
+    add_column :customer_maintenances, :remember_token, :string
   end
 end
